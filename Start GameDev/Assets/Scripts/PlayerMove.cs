6fb451b2b50c655d66fcb2110d8de1da46b9246d
samuelsouzaleite <128092642+samuelsouzaleite@users.noreply.github.com>
2024-03-20 15:20:58 -0300

    commit20032024

--- Start GameDev/Assets/Scripts/PlayerMove.cs
+++ Start GameDev/Assets/Scripts/PlayerMove.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float speed;
+    //public float speed;
+    //public float runSpeed;
 
+    [SerializeField]private float speed; //usando o SerializeField faz com que também fique público a variável na unity
+    [SerializeField]private float runSpeed;
+
+    private float initialSpeed;
     private Rigidbody2D rig;
     private Vector2 _direction;
 
@@ -18,14 +23,41 @@ public class PlayerMove : MonoBehaviour
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
     }
     private void Update()
     {
-        _direction = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        OnInput();
+        OnRun();
     }
 
     private void FixedUpdate()
     {
-        rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
+        OnMove();
     }
+
+    #region Movement
+
+        void OnInput()
+        {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        }
+        void OnMove()
+        {
+        rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
+        }
+
+        void OnRun()
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                speed = runSpeed;            
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                speed = initialSpeed;            
+            }
+        }
+    #endregion
 }
