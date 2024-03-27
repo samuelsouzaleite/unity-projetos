9d1e7ef4ead28b8b7b60307113c188747a5b3368
samuelsouzaleite <128092642+samuelsouzaleite@users.noreply.github.com>
2024-03-27 18:04:14 -0300

    commit27032024

---
+++ Start GameDev/Assets/Scripts/Dialogue/DialogueSettings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Dialogue", menuName = "New Dialogue/Dialogue")]
+public class DialogueSettings : ScriptableObject
+{
+  
+}
