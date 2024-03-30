0ae5a2bf4f0232e3523e315168d3dfcaa011dc8d
samuelsouzaleite <128092642+samuelsouzaleite@users.noreply.github.com>
2024-03-30 04:13:49 -0300

    commit

--- Start GameDev/Assets/Scripts/Dialogue/DialogueSettings.cs
+++ Start GameDev/Assets/Scripts/Dialogue/DialogueSettings.cs
@@ -5,5 +5,28 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="New Dialogue", menuName = "New Dialogue/Dialogue")]
 public class DialogueSettings : ScriptableObject
 {
-  
+    [Header("Settings")]
+    public GameObject actor;
+
+    [Header("Dialogue")]
+    public Sprite speakerSprite;
+    public string sentence;
+
+    public List<Sentences> dialogues = new List<Sentences>();
+}
+
+[System.Serializable]
+public class Sentences
+{
+    public string actorName;
+    public Sprite profile;
+    public Languages sentence;
+}
+
+[System.Serializable]
+public class Languages
+{
+    public string portuguese;
+    public string english;
+    public string spanish;
 }
