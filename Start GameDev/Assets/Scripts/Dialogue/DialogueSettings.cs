9377a81eddec7644834ac9edb7e49665a790d51d
samuelsouzaleite <contato.samuelsl@gmail.com>
2024-04-01 02:06:17 -0300

    commit

--- Start GameDev/Assets/Scripts/Dialogue/DialogueSettings.cs
+++ Start GameDev/Assets/Scripts/Dialogue/DialogueSettings.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor; 
 
 [CreateAssetMenu(fileName ="New Dialogue", menuName = "New Dialogue/Dialogue")]
 public class DialogueSettings : ScriptableObject
@@ -30,3 +31,35 @@ public class Languages
     public string english;
     public string spanish;
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(DialogueSettings))]
+public class BuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        DialogueSettings ds = (DialogueSettings)target;
+
+        Languages l = new Languages();
+        l.portuguese = ds.sentence;
+
+        Sentences s = new Sentences();
+        s.profile = ds.speakerSprite;
+        s.sentence = l;
+
+        if(GUILayout.Button("Create Dialogue"))
+        {
+            if(ds.sentence != "")
+            {
+                ds.dialogues.Add(s);
+
+                ds.speakerSprite = null;
+                ds.sentence = "";
+            }
+        }
+    }
+}
+
+#endif
