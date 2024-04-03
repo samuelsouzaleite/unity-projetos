0ba0437c35dd34280481b64625db0d9e316219e2
samuelsouzaleite <128092642+samuelsouzaleite@users.noreply.github.com>
2024-04-03 17:54:07 -0300

    commit-03.04.2024

--- Start GameDev/Assets/Scripts/Dialogue/DialogueControl.cs
+++ Start GameDev/Assets/Scripts/Dialogue/DialogueControl.cs
@@ -5,6 +5,16 @@ using UnityEngine.UI;
 
 public class DialogueControl : MonoBehaviour
 {
+    [System.Serializable]
+    public enum idiom
+    {
+        pt,
+        eng,
+        spa,
+    }
+
+    public idiom language;
+
     [Header("Components")]
     public GameObject dialogueObj; //Janela do dialogo
     public Image profileSprite; //sprite do perfil
@@ -15,17 +25,21 @@ public class DialogueControl : MonoBehaviour
     public float typingSpeed; //velocidade da fala
 
     //variáveis de controle
-    private bool isShowing; //se a janela esta visível
+    public bool isShowing; //se a janela esta visível - usando na frente da variável [HideInInspector] fará ela ser privada mesmo colocando como public
     private int index; //index das sentenças(falas/textos)
     private string[] sentences;
 
     public static DialogueControl instance;
 
+    //public bool IsShowing { get => isShowing; set => isShowing = value;  } //fará com que fique publico a variável booleana isShowing
+
+    //awake é chamado antes de todos os starts() na hierarquia de execução  de scripts
     private void Awake()
     {
         instance = this;
     }
 
+    //chamado ao inicializar
     void Start()
     {
         
@@ -48,7 +62,23 @@ public class DialogueControl : MonoBehaviour
     //pular próxima fase/fala
     public void NextSentence()
     {
-
+        if(speechText.text == sentences[index])
+        {
+            if(index < sentences.Length -1) 
+            {
+                index++;
+                speechText.text = "";
+                StartCoroutine(TypeSentence());
+            }
+            else //quando terminam os textos 
+            {
+                speechText.text = "";
+                index = 0;
+                dialogueObj.SetActive(false);
+                sentences = null;
+                isShowing = false;
+            }
+        }
     }
     //chamar a fala do npc
     public void Speech(string[] txt)
