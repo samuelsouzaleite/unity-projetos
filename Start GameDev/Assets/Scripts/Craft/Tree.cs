ca587095dcf3d2c39822704d2a121298c9056cbc
samuelsouzaleite <128092642+samuelsouzaleite@users.noreply.github.com>
2024-04-09 18:04:26 -0300

    commit.09.04.2024

--- Start GameDev/Assets/Scripts/Craft/Tree.cs
+++ Start GameDev/Assets/Scripts/Craft/Tree.cs
@@ -7,22 +7,36 @@ public class Tree : MonoBehaviour
     [SerializeField] private float treeHealth;
     [SerializeField] private Animator anim;
 
+    [SerializeField] private GameObject woodPrefab;
+    [SerializeField] private int totalWood;
+
+    [SerializeField] private ParticleSystem leafs;
+
+    private bool isCut; //todo booleano por padrão ele ja vem false
+
     public void OnHit()
     {
         treeHealth--;
 
         anim.SetTrigger("isHit");
+        leafs.Play();
 
         if (treeHealth <= 0) 
         {
             //cria o toco e instancia os drops (madeira)
+            for (int i = 0; i < totalWood; i++)
+            {
+            Instantiate(woodPrefab, transform.position + new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0f), transform.rotation);
+            }
             anim.SetTrigger("cut");
+
+            isCut = true;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Axe"))
+        if(collision.CompareTag("Axe") && !isCut)
         {
             OnHit();
         }
