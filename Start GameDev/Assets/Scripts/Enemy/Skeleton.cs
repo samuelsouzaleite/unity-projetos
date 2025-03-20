807b4f356b50ba37f0a37eed00ad7ca7688dba14
samuelsouzaleite <contato.samuelsl@gmail.com>
2025-03-20 18:14:46 -0300

    commitSkeletonMove

--- Start GameDev/Assets/Scripts/Enemy/Skeleton.cs
+++ Start GameDev/Assets/Scripts/Enemy/Skeleton.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Skeleton : MonoBehaviour
 {
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] Transform target;
+    [SerializeField] private AnimationControl animControl;
+
+    private PlayerMove player;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        player = FindObjectOfType<PlayerMove>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(target.position);
+
+        if (Vector2.Distance(transform.position, target.position) <= agent.stoppingDistance)
+        {
+            //chegou no limite de distância /skeleton para
+            animControl.PlayAnim(2);
+
+        }
+
+        else
+        {
+            //skeleton segue o player
+            animControl.PlayAnim(1);
+
+        }
+
+        float posX = player.transform.position.x - transform.position.x;
+        if (posX > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
     }
 }
